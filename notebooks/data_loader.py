c7cee237cad76d12b17f1a8d98d828de7aeee0b0
Kazi Noshin <epw9kz@portal07.cs.Virginia.EDU>
2023-11-05 21:34:56 -0500

    data loader updated

--- notebooks/data_loader.py
+++ notebooks/data_loader.py
@@ -1,6 +1,23 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+
+def preprocessing(df):
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            labelencoder = LabelEncoder()
+            df[col] = labelencoder.fit_transform(df[col])
+
+    # print(df.info())
+    # normalizing
+    scaler = MinMaxScaler()
+    scaler.fit(df)
+    df = scaler.transform(df)
+
+    return df
+            
 
 def load_txt(file_no):
     path_name = '/u/epw9kz/academic/first_year/ML/project/'
@@ -13,14 +30,16 @@ def load_txt(file_no):
         pass
         
     df = pd.read_csv(file, sep='\t', header=None)
-    
-    X = df.iloc[:, :-1].values
-    y = df.iloc[:, -1].values
+
+    df = preprocessing(df)
+
+    X = df[:, :-1]
+    y = df[:, -1]
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
     
     return X_train, X_test, y_train, y_test 
 
-# X_train, X_test, y_train, y_test = load_txt(1) 
-# print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+X_train, X_test, y_train, y_test = load_txt(2) 
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
     
